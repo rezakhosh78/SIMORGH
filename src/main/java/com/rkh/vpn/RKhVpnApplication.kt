917c0ae303c4b27b45a53aae0fd9c4a68f85c129
ReZa Kh <115151121+rezakhosh78@users.noreply.github.com>
2026-06-13 04:56:07 +0330

    Delete src/main/java/com/rkh/vpn/RKhVpnApplication.kt

--- src/main/java/com/rkh/vpn/RKhVpnApplication.kt
+++
@@ -1,4 +0,0 @@
-package com.rkh.vpn
-import android.app.Application
-import com.rkh.vpn.service.NotificationHelper
-class RKhVpnApplication: Application(){ override fun onCreate(){ super.onCreate(); NotificationHelper.createChannels(this) } }
